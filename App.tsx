4690addd79a56f9c5ff270e8da0eb751c1b5723c
Avishay Elihay <xd.AElihay@idf.il>
2024-04-08 15:45:46 +0300

    keyboardavoidingview

--- App.tsx
+++ App.tsx
@@ -5,15 +5,18 @@ import AppNavigator from "./navigation/AppNavigator";
 import { persistor, store } from "./redux/app/store";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { PersistGate } from "redux-persist/integration/react";
+import { KeyboardAvoidingView } from "react-native";
 
 export default function App() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <AppNavigator />
-        </PersistGate>
-      </Provider>
+      <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <AppNavigator />
+          </PersistGate>
+        </Provider>
+      </KeyboardAvoidingView>
     </GestureHandlerRootView>
   );
 }
